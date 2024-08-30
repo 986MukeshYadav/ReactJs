9ab1f56804332143ac4aa14fd8775b35e6157493
Mukesh <myadav986@rku.ac.in>
2024-08-30 17:08:09 +0530

    card made

--- first-website/src/App.jsx
+++ first-website/src/App.jsx
@@ -1,3 +1,11 @@
 export const App=()=>{
-  return <h1>Hello Mukesh</h1>
+  return (<div>
+    <div>
+      <img src="img.png" alt="" width="40%" height="40%"/>
+    </div>
+   <h1>Queen of Nepal</h1>
+  <h2>Rating: 8.5</h2>
+  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad iure reprehenderit adipisci neque iusto minima magnam impedit nisi possimus corporis incidunt, eligendi sint cum saepe ab a porro necessitatibus repudiandae dolorum vero nobis maxime provident, eveniet soluta! Ut, perferendis enim tempora cum, porro ea saepe, repellat recusandae dolore aliquam minus.</p>
+  </div>
+  );
 }
