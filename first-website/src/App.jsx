146317571d5cc1b07ab50f1d877436c9b202fa2b
Mukesh <myadav986@rku.ac.in>
2024-08-31 16:35:34 +0530

    conditional rendering with one interview question

--- first-website/src/App.jsx
+++ first-website/src/App.jsx
@@ -1,11 +1,65 @@
 export const App=()=>{
-  return (<div>
+  // return ([<Netfliex key="1"/>,<Netfliex key="2"/>]
+  return(
+    <>
+  <Netfliex/>
+  <Netfliex/>
+  <Netfliex/>
+  <Netfliex/>
+  <Netfliex/>
+  <Netfliex/>
+  </>
+  );
+}
+
+const Netfliex=()=>{
+  const name="Queen of Nepal";
+  // const rating=8.5;
+  const description="Queen of Nepal is a 2020 Indian Hindi-language romantic drama film directed by Rahul Kapoor and produced by Kiran Kapoor and Ashok Kapoor" 
+  let age = 16;
+
+ let canWatch = "Not Avialable";
+  if(age >= 18) canWatch = "Watch Now";
+
+//  const canWatch=()=>{
+//   if(age >= 18) return "Watch Now";
+//   return "Not Available";
+//  }
+
+  const movieGenre=()=>{
+    const genre="Comedy Movie";
+    return genre;
+  }
+  // if(age < 18 ){
+  //   return (
+  //     <div>
+  //     <div>
+  //       <img src="img.png" alt="" width="40%" height="40%"/>
+  //     </div>
+  //    <h1>Name:{name}</h1>   {/*Dynamic value using value */}
+  //   <h2>Rating:{5+8}</h2>   {/*Dynamic value using Expression */}
+  //   <p>Content:{description}</p>
+  //   <p>Genre:{movieGenre()}</p>  {/*Dynamic value using function */}
+  //   <button>Not Avaliable</button>
+  //   </div>
+  //   )
+  // }
+
+  return(
+    <div>
     <div>
       <img src="img.png" alt="" width="40%" height="40%"/>
     </div>
-   <h1>Queen of Nepal</h1>
-  <h2>Rating: 8.5</h2>
-  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad iure reprehenderit adipisci neque iusto minima magnam impedit nisi possimus corporis incidunt, eligendi sint cum saepe ab a porro necessitatibus repudiandae dolorum vero nobis maxime provident, eveniet soluta! Ut, perferendis enim tempora cum, porro ea saepe, repellat recusandae dolore aliquam minus.</p>
+   <h1>Name:{name}</h1>   {/*Dynamic value using value */}
+  <h2>Rating:{5+8}</h2>   {/*Dynamic value using Expression */}
+  <p>Content:{description}</p>
+  <p>Genre:{movieGenre()}</p>  {/*Dynamic value using function */}
+
+  {/* <button>{age >= 18 ? "Watch Now":"Not Avaliable"}</button> */}
+
+    {/* <button>{canWatch()}</button> */}
+
+    <button>{canWatch}</button>
   </div>
-  );
+  )
 }
---
+++ first-website/src/practice.jsx
@@ -0,0 +1,17 @@
+export const Practice=()=>{
+    const student=[];
+    console.log(student.length)
+    return(
+        <>
+        {/* 1st */}
+        {/* <p>{student.length === 0 && "No students Found"}</p> */}
+
+        {/* 2nd */}
+        {/* <p>{!student.length  && "No students Found"}</p> */}
+
+        {/* 3rd */}
+        <p>{Boolean(student.length)  && "No students Found"}</p>
+        <p>Numbers of students:{student.length}</p>
+        </>
+    )
+}
