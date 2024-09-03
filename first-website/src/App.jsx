43052b3f01cf54a2bb3b4fba17cad6de5a3c282f
Mukesh <myadav986@rku.ac.in>
2024-09-03 17:36:47 +0530

    more video completed

--- first-website/src/App.jsx
+++ first-website/src/App.jsx
@@ -1,10 +1,12 @@
 import NetfliexsSeries from "./components/NetfliexsSeries";
-import "./components/Netfliexs.css"
+// import "./components/Netfliexs.css"
+import "./components/Netfliexs.module.css"
+import styles from "./components/Netfliexs.module.css";
 
 export const App=()=>{
   return(
     <section className="container">
-      <h1 className="card-heading">List of best Netfliex Series</h1>
+      <h1 className={styles["card-heading"]}>List of best Netfliex Series</h1>
     <NetfliexsSeries/>
   </section>
   );
--- first-website/src/components/SeriesCard.jsx
+++ first-website/src/components/SeriesCard.jsx
@@ -1,31 +1,69 @@
+import styles from "./Netfliexs.module.css";
+import styled from 'styled-components';
 export const SeriesCard=(props)=>{
     const {name,rating,description,genre,cast,watch_url,img_url} = props.series
 
-    const styleContent={
-      padding:"1.2rem 2.4rem",
-      border:"none",
-      fontSize:"1.6rem",
-      backgroundColor: rating >= 8.5 ? "#7dcea0": "#f7dc6f",
-      color:"var(--h-color)",
-      font:"bold",
-      cursor:"pointer",
+    // const styleContent={
+    //   padding:"1.2rem 2.4rem",
+    //   border:"none",
+    //   fontSize:"1.6rem",
+    //   backgroundColor: rating >= 8.5 ? "#7dcea0": "#f7dc6f",
+    //   color:"var(--h-color)",
+    //   font:"bold",
+    //   cursor:"pointer",
       
-    }
+    // }
 
-    const ratingContent= rating >= 8.5 ? "super_hit": "average"
+// const ButtonMukesh=styled.button({
+//       padding:"1.2rem 2.4rem",
+//       border:"none",
+//       fontSize:"1.6rem",
+//       backgroundColor: rating >= 8.5 ? "#7dcea0": "#f7dc6f",
+//       color:"var(--h-color)",
+//       font:"bold",
+//       cursor:"pointer",
+// })
+
+const ButtonMukesh = styled.button`
+padding: 1.2rem 2.4rem;
+border: none;
+font-size: 1.6rem;
+background-color: ${(props) =>
+  props.rating >= 8.5 ? "#7dcea0" : "#f7dc6f"};
+color: var(--btn-color);
+font-weight: bold;
+cursor: pointer;
+`;
+
+  //  In styled-components, you can pass a function within the template literal to dynamically set CSS properties based on props or state.
+
+  const Rating = styled.h3`
+    font-size: 1.6rem;
+    color: #7dcea0;
+    text-transform: capitalize;
+  `;
+
+
+
+
+    const ratingContent= rating >= 8.5 ? styles.super_hit: styles.average
 
 return (
-    <li className="card">
+    <li className={styles.card}>
     <div>
               <img  src={img_url} alt="" width="40%" height="40%" />
-            <div className="card-content">
+            <div className={styles["card-content"]}>
             <h1>Name:{name}</h1>
-            <h2>Rating:<span className={`rating ${ratingContent}`}>{rating} </span></h2>
+            <Rating>
+          Rating:
+          <span className={`${styles.rating} ${ratingContent}`}>{rating}</span>
+        </Rating>
             <p>Description:{description}</p>
             <p>Genre:{genre}</p>
             <p>Cast:{cast}</p>
             <a href={watch_url} target="_blank">
-              <button style={styleContent} className="btn">Watch on </button>
+              {/* <button style={styleContent} className="btn">Watch on </button> */}
+              <ButtonMukesh  rating={rating}>Watch on </ButtonMukesh>
             </a>
             </div>
      </div>
